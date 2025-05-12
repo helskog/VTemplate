f5305dad411cefdfa938c33aa0f399e7677817b8
helskog <simonhelskog@outlook.com>
2025-05-12 02:48:13 +0200

    template edits & nuget.config addition

--- VTemplate/setup_git.sh
+++ VTemplate/setup_git.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-
 set -e
 
-echo "Initializing Git repo..."
+REPO_NAME="$(basename "$(pwd)")"
+GITHUB_USER="$(gh api user --jq .login)"
 
+echo "Initializing Git repo"
 git init
+git checkout -b dev
 git add .
 git commit -m "Initial commit"
 
-echo "Creating GitHub repo..."
+echo "Creating GitHub repo"
+gh repo create "$REPO_NAME" --private --source=. --remote=origin --push
 
-gh repo create "$(basename "$(pwd)")" --private --source=. --remote=origin --push
+echo "Pushing dev branch"
+git push -u origin dev
+
+echo "Creating empty release branch"
+git checkout --orphan release
+
+echo "Switching back to dev branch"
+git checkout dev
+
+echo "Adding GitHub Actions secret..."
+gh secret set THUNDERSTORE_SECRET --repo "$GITHUB_USER/$REPO_NAME" --body "$DEPLOY_TS_SECRET"
+
+echo "Finished all actions!"
